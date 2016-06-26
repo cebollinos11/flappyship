b60f724ccac45ff752f3abd71c3a53e6bf353c7c
cebollinos11 <cebollinos@gmail.com>
2016-06-26 17:00:16 +0200

    oscillating ships

---
+++ Assets/oscillateShip.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class oscillateShip : MonoBehaviour {
+
+   
+
+    float speed, amplitude;
+
+	// Use this for initialization
+	void Start () {
+        speed = 1f;
+        amplitude = 0.4f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.forward * Mathf.Cos(Time.time * speed) * amplitude);
+	}
+}
